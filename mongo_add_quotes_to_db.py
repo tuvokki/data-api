008f4fcbdded156efe0a39240b6dcb21b2d373ad
Wouter <tuvokki@gmail.com>
2015-04-27 09:43:35 +0200

    Change data

--- mongo_add_quotes_to_db.py
+++ mongo_add_quotes_to_db.py
@@ -12,71 +12,71 @@ class TestMongoInsertFunctions(unittest.TestCase):
   def testConn(self):
     quotes = [
       {
-        'saying': "Waar ik inmiddels behoorlijk zenuwachtig van wordt is het lot van Jip en Janneke.",
+        'text': "Waar ik inmiddels behoorlijk zenuwachtig van wordt is het lot van Jip en Janneke.",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Since there is absolutely no logical reason to assume there is an afterlife, I decided to make the life I have now as much fun as possible.",
+        'text': "Since there is absolutely no logical reason to assume there is an afterlife, I decided to make the life I have now as much fun as possible.",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Ik heb besloten om me terug te trekken uit het voetbal en ga me volledig inzetten voor het supporterschap van de wielerploeg Francaise des Jeux!",
+        'text': "Ik heb besloten om me terug te trekken uit het voetbal en ga me volledig inzetten voor het supporterschap van de wielerploeg Francaise des Jeux!",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Blijkbaar leidt in de politiek een zoen tot meer ophef dan fraude! Zo komt het natuurlijk nooit goed met Nederland!",
+        'text': "Blijkbaar leidt in de politiek een zoen tot meer ophef dan fraude! Zo komt het natuurlijk nooit goed met Nederland!",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Een vrolijke vrind is U goed gezind!",
+        'text': "Een vrolijke vrind is U goed gezind!",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Je oude rommel verkopen op Koningsdag! Dat kan niet anders dan een Republikein zijn die dat heeft verzonnen!",
+        'text': "Je oude rommel verkopen op Koningsdag! Dat kan niet anders dan een Republikein zijn die dat heeft verzonnen!",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Op papier is Messi de beste speler ter wereld. Vanavond spelen we op gras.",
+        'text': "Op papier is Messi de beste speler ter wereld. Vanavond spelen we op gras.",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Als je je pink in je oor doet, en dan op en neer gaat, klinkt het als Pacman. Toch??",
+        'text': "Als je je pink in je oor doet, en dan op en neer gaat, klinkt het als Pacman. Toch??",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Everyone you meet is fighting a battle you know nothing about. That's why u must be kind always!",
+        'text': "Everyone you meet is fighting a battle you know nothing about. That's why u must be kind always!",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "RIVM waarschuwt geen rauw vlees te eten uit Ebola-landen. Nou had ik toch al niet zo'n trek in rauwe Liberiaanse geit.",
+        'text': "RIVM waarschuwt geen rauw vlees te eten uit Ebola-landen. Nou had ik toch al niet zo'n trek in rauwe Liberiaanse geit.",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Eindelijk weer eens tijd om iets op Facebook te plaatsen! ....... Dat was het dan weer!! Tot de volgende keer!",
+        'text': "Eindelijk weer eens tijd om iets op Facebook te plaatsen! ....... Dat was het dan weer!! Tot de volgende keer!",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Het voordeel van in een vrouwengezin leven is dat je als een echte prinses behandeld wordt!!! ",
+        'text': "Het voordeel van in een vrouwengezin leven is dat je als een echte prinses behandeld wordt!!! ",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Tuurlijk heb ik mening! :) Niet vies van Erik van Wunnik marketing! ;)",
+        'text': "Tuurlijk heb ik mening! :) Niet vies van Erik van Wunnik marketing! ;)",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "A library makes me smart on a photo!",
+        'text': "A library makes me smart on a photo!",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "Iedereen veel drank, liefde en goeie muziek de komende feestdagen! Ik zeker......",
+        'text': "Iedereen veel drank, liefde en goeie muziek de komende feestdagen! Ik zeker......",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "I will not follow where the path may lead, but I will go where there is no path, and I will leave a trail.",
+        'text': "I will not follow where the path may lead, but I will go where there is no path, and I will leave a trail.",
         'who': "Erik van Wunnik"
       },
       {
-        'saying': "The more future to come, the more customers to satisfy",
+        'text': "The more future to come, the more customers to satisfy",
         'who': "Erik van Wunnik"
       }
     ]
