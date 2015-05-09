16ad7989cb941e4774d3c965902611c18b5bc882
Wouter <tuvokki@gmail.com>
2015-05-09 21:04:16 +0200

    Use env for mongo settings

--- modules/quotes.py
+++ modules/quotes.py
@@ -1,11 +1,13 @@
 from modules import *
+import os
 from random import choice
 from pymongo import MongoClient
 from bson.json_util import dumps
 
 class QuotesView(FlaskView):
   # Get a reference to the MongoDB
-  client = MongoClient('mongodb://localhost:27017/')
+  connect_string = 'mongodb://%s:%s/' % (os.getenv('MONGO_HOST', 'localhost'), os.getenv('MONGO_PORT', '27017'))
+  client = MongoClient(connect_string)
   db = client['quotes']
   collection = db['simple']
 
