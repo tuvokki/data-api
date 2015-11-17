5f65b88cc95c6a25b8eff62d73298d81a0b265a7
Wouter Roosendaal <tuvokki@gmail.com>
2015-11-17 17:10:17 +0100

    Add  some content

--- static/app.js
+++ static/app.js
@@ -1,15 +1,41 @@
-var myApp = angular.module('myApp', ['ui.router']);
+var myApp = angular.module('myApp', ['ui.router', 'ui.bootstrap']);
+
+myApp.controller('CarouselDemoCtrl', function () {
+  this.myInterval = 0; //disable autoscrolling
+  this.noWrapSlides = true; //stop at the last slide
+  var slides = this.slides = [];
+  slides.push(
+    { image: '/static/slides/prepare_for_the_future.png',
+      text: 'Prepareforthefuture'
+    },
+    { image: '/static/slides/recommendations.png',
+      text: 'Recommendations'
+    },
+    { image: '/static/slides/upgrade.png',
+      text: 'Upgrade'
+    },
+    { image: '/static/slides/questions.png',
+      text: 'Questions?'
+    }
+  );
+})
+
 myApp.config(function($stateProvider, $urlRouterProvider) {
   //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/state1");
+  // For any unmatched url, redirect to the home state at /
+  $urlRouterProvider.otherwise("/");
   //
   // Now set up the states
   $stateProvider
+    .state('home', {
+      url: "/",
+      controller: 'CarouselDemoCtrl as carousel',
+      templateUrl: "partials/home.html"
+    })
     .state('state1', {
       url: "/state1",
       controller: function($scope) {
-        $scope.test = "People, this is a test";
+        $scope.whatever = "People, this is a test";
       },
       templateUrl: "partials/state1.html"
     })
@@ -22,9 +48,10 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
     })
     .state('state2', {
       url: "/state2",
-      controller: function($scope) {
-        $scope.test = "People, this is a tosti";
+      controller: function() {
+        this.whatever = "People, this is a tosti";
       },
+      controllerAs: 'test',
       templateUrl: "partials/state2.html"
     })
     .state('state2.list', {
