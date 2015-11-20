f83dea8a58552e03d7795ed6ff89a26733fde566
Wouter <tuvokki@gmail.com>
2015-11-20 20:24:41 +0100

    Fix path errors on osx

--- modules/slides.py
+++ modules/slides.py
@@ -1,15 +1,17 @@
 from modules import *
 import os
-from bson.json_util import dumps
+# from bson.json_util import dumps
 
 
 class SlidesView(FlaskView):
-  def index(self):
-      result = []
-      for file in os.listdir(os.path.dirname(__file__) + '\..\static\slides\\'):
-          result.append({
-            'image': '/static/slides/' + file,
-            'text': 'Slide'
-          })
-      return jsonify(slides=result)
 
+    def index(self):
+        result = []
+        our_path = os.path.dirname(__file__)
+        dir_path = os.path.join(our_path, "..", "static", "slides")
+        for file in os.listdir(dir_path):
+            result.append({
+                'image': '/static/slides/' + file,
+                'text': 'slide'
+            })
+        return jsonify(slides=result)
