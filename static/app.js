45846b7fb8db7ac56c9f29ba5e920ff38eaf2e26
Wouter <tuvokki@gmail.com>
2015-11-21 16:41:40 +0100

    Gridshow component

--- static/app.js
+++ static/app.js
@@ -13,18 +13,84 @@ myApp.factory('Slides', function($http) {
 
 });
 
+myApp.component('gridshow', {
+  bindings: {
+    slides: '='
+  },
+  controller: function($scope) {
+    var self = this;
+    this.rows = [];
+    console.log('test', this);
+
+    $scope.$watch(angular.bind(this, function () { 
+        if (this.slides) {
+          this.rows = [];
+          var tmpObject = [];
+          var counter = 1;
+          for (var i = 0; i < this.slides.length; i++) {
+            tmpObject.push(this.slides[i].image);
+            if (counter++ == 4){
+              self.rows.push({
+                title: 'Row ' + this.rows.length,
+                images: tmpObject
+              });
+              counter = 1;
+              tmpObject = [];
+            }
+          };
+          self.rows.push({
+                title: 'Row ' + this.rows.length,
+                images: tmpObject
+              });
+        }
+        return this.slides;
+    }), function(value) {
+        console.log('Name change to ' + value);
+    });
+  },
+  controllerAs: 'grid',
+  templateUrl: 'partials/showgrid.html'
+});
+
+myApp.component('counter', {
+  bindings: {
+    count: '='
+  },
+  controller: function () {
+    function increment() {
+      this.count++;
+    }
+    function decrement() {
+      this.count--;
+    }
+    this.increment = increment;
+    this.decrement = decrement;
+  },
+  controllerAs: 'klapsigaar',
+  template: function ($element, $attrs) {
+    // access to $element and $attrs
+    return [
+      '<div class="todo">',
+        '<input type="text" ng-model="klapsigaar.count">',
+        '<button type="button" ng-click="klapsigaar.decrement();">-</button>',
+        '<button type="button" ng-click="klapsigaar.increment();">+</button>',
+      '</div>'
+    ].join('')
+  }
+});
+
 myApp.controller('CarouselDemoCtrl', function($log, Slides) {
   var self = this;
   this.myInterval = 0; //disable autoscrolling
   this.noWrapSlides = true; //stop at the last slide
   
   Slides.getSlides().then(function(slides) {
-    $log.debug(slides.data.slides);
+    //$log.debug(slides.data.slides);
     self.slides = slides.data.slides;
   }, function errorCallback(response) {
     $log.error('no slides: ', response)
   });
-})
+});
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
   //
@@ -38,33 +104,42 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
       controller: 'CarouselDemoCtrl as carousel',
       templateUrl: "partials/home.html"
     })
-    .state('state1', {
-      url: "/state1",
-      controller: function($scope) {
-        $scope.whatever = "People, this is a test";
+    .state('examples', {
+      url: "/examples",
+      controller: function($log, Slides) {
+        var self = this;
+        self.count = 8;
+        self.whatever = "People, this is a test " + self.count;
+        Slides.getSlides().then(function(slides) {
+            //$log.debug(slides.data.slides);
+            self.slidelist = slides.data.slides;
+          }, function errorCallback(response) {
+            $log.error('no slides: ', response)
+          });
       },
-      templateUrl: "partials/state1.html"
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "partials/state1.list.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    .state('state2', {
-      url: "/state2",
-      controller: function() {
-        this.whatever = "People, this is a tosti";
-      },
-      controllerAs: 'test',
-      templateUrl: "partials/state2.html"
-    })
-    .state('state2.list', {
-      url: "/list",
-      templateUrl: "partials/state2.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
-      }
+      controllerAs: 'examples',
+      templateUrl: "partials/examples.html"
+    // })
+    // .state('examples.list', {
+    //   url: "/list",
+    //   templateUrl: "partials/examples.list.html",
+    //   controller: function($scope) {
+    //     $scope.items = ["A", "List", "Of", "Items"];
+    //   }
+    // })
+    // .state('state2', {
+    //   url: "/state2",
+    //   controller: function() {
+    //     this.whatever = "People, this is a tosti";
+    //   },
+    //   controllerAs: 'test',
+    //   templateUrl: "partials/state2.html"
+    // })
+    // .state('state2.list', {
+    //   url: "/list",
+    //   templateUrl: "partials/state2.list.html",
+    //   controller: function($scope) {
+    //     $scope.things = ["A", "Set", "Of", "Things"];
+    //   }
     });
 });
